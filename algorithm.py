8c4d54ad941bd56c5c5fdafd3738e3aba4f5d77f
Nicholas Grasevski <nicholas.grasevski@rokt.com>
2020-10-18 11:27:12 +1100

    added other algorithms

--- algorithm.py
+++ algorithm.py
@@ -87,7 +87,7 @@ def plot(red: int, imgsize: Tuple[int, int], *images: np.ndarray) -> None:
     plt.show()
 
 
-def nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def nmf_baseline(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """Basic NMF algorithm, using sklearn library"""
     model = NMF(n_components=K)
     W = model.fit_transform(X)
@@ -95,66 +95,101 @@ def nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     return W, H
 
 
-@njit(fastmath=True, parallel=True)
-def nmf_beta(K: int,
-             X: np.ndarray,
-             beta: float = 2,
-             l1: float = 1e-2,
-             l2: float = 0,
-             steps: int = 200,
-             tol: float = 1e-4) -> Tuple[np.ndarray, np.ndarray]:
-    """Algorithms for nonnegative matrix factorization with the B divergence"""
+def nmf(K: int,
+        X: np.ndarray,
+        beta: float = 2,
+        l1: float = 0,
+        l2: float = 0,
+        weight: Callable[[np.ndarray, np.ndarray, np.ndarray],
+                         np.ndarray] = lambda x, w, h: 1,
+        steps: int = 100,
+        tol: float = 1e-4) -> Tuple[np.ndarray, np.ndarray]:
+    """Generic NMF algorithm using multiplicative updates"""
     avg = np.sqrt(X.mean() / K)
     W, H = avg * np.random.rand(len(X), K), avg * np.random.rand(K, len(X[0]))
     for _ in range(steps):
-        WH = W @ H
-        d_W = (X *
-               (WH**(beta - 2))) @ H.T / ((WH**(beta - 1)) @ H.T + l1 + l2 * W)
-        e_W = np.linalg.norm(W * (1 - d_W))
-        W *= d_W
-        WH = W @ H
-        d_H = W.T @ (X *
-                     (WH**(beta - 2))) / (W.T @ (WH**(beta - 1)) + l1 + l2 * H)
-        H *= d_H
-        e_H = np.linalg.norm(H * (1 - d_H))
-        if e_W < tol and e_H < tol:
+        W, H, done = mur(X, W, H, weight(X, W, H), beta, l1, l2, tol)
+        if done:
             break
     return W, H
 
 
 @njit(fastmath=True, parallel=True)
-def nmf_tanh(K: int,
-             X: np.ndarray,
-             p: float = 1,
-             l1: float = 1e-2,
-             l2: float = 1e-2,
-             y: float = 0,
-             steps: int = 200,
-             tol: float = 1e-4) -> Tuple[np.ndarray, np.ndarray]:
+def mur(X: np.ndarray, W: np.ndarray, H: np.ndarray, U: np.ndarray,
+        beta: float, l1: float, l2: float,
+        tol: float) -> Tuple[np.ndarray, np.ndarray, bool]:
+    """Weighted multiplicative update rule"""
+    WH = W @ H
+    d_W = U * X * WH**(beta - 2) @ H.T / (U * WH**(beta - 1) @ H.T + l1 +
+                                          l2 * W)
+    e_W = np.linalg.norm(W * (1 - d_W))
+    W *= d_W
+    WH = W @ H
+    d_H = W.T @ (U * X * WH**(beta - 2)) / (W.T @ (U * WH**(beta - 1)) + l1 +
+                                            l2 * H)
+    H *= d_H
+    e_H = np.linalg.norm(H * (1 - d_H))
+    return W, H, e_W < tol and e_H < tol
+
+
+@njit(fastmath=True, parallel=True)
+def err(X: np.ndarray, W: np.ndarray, H: np.ndarray) -> np.ndarray:
+    """Difference between the original and the reconstructed"""
+    return X - W @ H
+
+
+@njit(fastmath=True, parallel=True)
+def tanh_weight(X: np.ndarray,
+                W: np.ndarray,
+                H: np.ndarray,
+                p: float = 1) -> np.ndarray:
+    """Weight calculation for tanhNMF"""
+    E = err(X, W, H)
+    a = X.size * p / (E**2).sum()
+    return a * (1 - np.tanh(a * np.abs(E))**2)
+
+
+@njit(fastmath=True, parallel=True)
+def cim_weight(X: np.ndarray, W: np.ndarray, H: np.ndarray) -> np.ndarray:
+    """Weight calculation for CIM NMF"""
+    E2 = err(X, W, H)**2
+    return np.exp(-E2 / E2.mean())
+
+
+@njit(fastmath=True)
+def l1_weight(X: np.ndarray, W: np.ndarray, H: np.ndarray) -> np.ndarray:
+    """Weight calculation for l1 NMF"""
+    return 1 / np.linalg.norm(err(X, W, H), ord=1)
+
+
+def l21_weight(X: np.ndarray, W: np.ndarray, H: np.ndarray) -> np.ndarray:
+    """Weight calculation for l21 NMF"""
+    return 1 / np.linalg.norm(err(X, W, H), axis=0)
+
+
+def tanh_nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """Another Robust NMF"""
-    avg = np.sqrt(X.mean() / K)
-    W, H = avg * np.random.rand(len(X), K), avg * np.random.rand(K, len(X[0]))
-    D = np.zeros(H.shape)
-    for _ in range(steps):
-        if y:
-            for i in range(len(D)):
-                for j in range(len(D[0])):
-                    D[i][j] = np.exp(-np.linalg.norm(X.T[j] - W.T[i]))
-        E = X - W @ H
-        a = X.size * p / (E**2).sum()
-        U = a * (1 - np.tanh(a * np.abs(E))**2)
-        HD2 = (H * D)**2
-        d_W = (U * X @ H.T + 2 * y * X @ HD2.T) / (
-            (U * (W @ H)) @ H.T + 2 * y * W * HD2.sum(axis=1) + l1 + l2 * W)
-        e_W = np.linalg.norm(W * (1 - d_W))
-        W *= d_W
-        d_H = W.T @ (U * X) / (W.T @ (U *
-                                      (W @ H)) + y * H * D * D + l1 + l2 * H)
-        e_H = np.linalg.norm(H * (1 - d_H))
-        H *= d_H
-        if e_W < tol and e_H < tol:
-            break
-    return W, H
+    return nmf(K, X, l2=1e-2, weight=tanh_weight)
+
+
+def cim_nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Robust NMF via half-quadratic minimization"""
+    return nmf(K, X, weight=cim_weight)
+
+
+def l21_nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Robust nonnegative matrix factorization using l21-norm"""
+    return nmf(K, X, weight=l21_weight)
+
+
+def l1_nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """“Non-negative Matrix Factorization for Images with Laplacian Noise"""
+    return nmf(K, X, weight=l1_weight)
+
+
+def kl_nmf(K: int, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Algorithms for nonnegative matrix factorization with the β-divergence"""
+    return nmf(K, X, beta=1)
 
 
 def no_noise(shape: Tuple[int, int]) -> float:
@@ -171,19 +206,19 @@ def salt_and_pepper(shape: Tuple[int, int],
     return 1 * (p_noise * r_noise) - 1 * (p_noise * ~r_noise)
 
 
-def uniform(shape: Tuple[int, int], scale: float = 0.4) -> np.ndarray:
+def uniform(shape: Tuple[int, int], scale: float = 0.1) -> np.ndarray:
     """Uniform noise"""
     return scale * (np.random.rand(*shape) - 0.5)
 
 
 def laplace(shape: Tuple[int, int]) -> np.ndarray:
     """Laplace noise"""
-    return np.random.laplace(0.5, 0.4)
+    return np.random.laplace(0.5, 0.1)
 
 
 def gaussian(shape: Tuple[int, int]) -> np.ndarray:
     """Gaussian noise"""
-    return np.random.normal(0.5, 0.4)
+    return np.random.normal(0.5, 0.1)
 
 
 def evaluate_algorithm(
@@ -210,6 +245,9 @@ def run_nmf_algorithms(w: DictWriter, w_summary: DictWriter) -> None:
     rre = np.zeros(len(Y_hats))
     acc, nmi = rre.copy(), rre.copy()
     np.random.seed(0)
+    algorithms = [
+        nmf_baseline, nmf, kl_nmf, l1_nmf, l21_nmf, cim_nmf, tanh_nmf
+    ]
 
     for dataset, red, imgsize in ('ORL', 3, (92, 112)), ('CroppedYaleB', 4,
                                                          (168, 192)):
@@ -226,30 +264,36 @@ def run_nmf_algorithms(w: DictWriter, w_summary: DictWriter) -> None:
         for noise in no_noise, salt_and_pepper, uniform, laplace, gaussian:
             # Add Noise
             Vs = [np.clip(v + noise(v.shape), 0, 1) for v in V_hats]
+
             plt.figure(figsize=(10, 3))
-            plt.suptitle(f'{dataset} dataset with {noise.__name__} noise')
             ind = 2  # index of demo image
-            NUM_ALGORITHMS = 4
+
+            row = {
+                'dataset': dataset,
+                'noise': noise.__name__.replace('_', '-'),
+            }
 
             for i, v in enumerate(Vs):
-                plt.subplot(len(Vs), NUM_ALGORITHMS, NUM_ALGORITHMS * i + 1)
+                plt.subplot(len(Vs),
+                            len(algorithms) + 1, (len(algorithms) + 1) * i + 1)
+                if i == 0:
+                    plt.title('input')
                 plt.imshow(SCALE * v[:, ind].reshape(img_size[1], img_size[0]),
                            cmap=plt.cm.gray)
                 plt.xticks(())
                 plt.yticks(())
 
-            for (a, algorithm) in enumerate((nmf, nmf_beta, nmf_tanh), 1):
-                row = {
-                    'dataset': dataset,
-                    'noise': noise.__name__,
-                    'algorithm': algorithm.__name__
-                }
+            for a, algorithm in enumerate(algorithms, 1):
+                row['algorithm'] = algorithm.__name__.replace('_', '-')
 
                 for i, (V, V_hat, Y_hat) in enumerate(zip(Vs, V_hats, Y_hats)):
                     rre[i], acc[i], nmi[i], W, H = evaluate_algorithm(
                         V, V_hat, Y_hat, algorithm)
-                    plt.subplot(len(Vs), NUM_ALGORITHMS,
-                                NUM_ALGORITHMS * i + a + 1)
+                    plt.subplot(len(Vs),
+                                len(algorithms) + 1,
+                                (len(algorithms) + 1) * i + a + 1)
+                    if i == 0:
+                        plt.title(row['algorithm'])
                     plt.imshow(
                         SCALE *
                         (W @ H)[:, ind].reshape(img_size[1], img_size[0]),
